a1cb6837fa0e8b8ac50846ac5b7eb03146317a79
Rafael Nascimento de Souza Leão <rleaodev@gmail.com>
2019-10-13 23:03:30 -0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link :to="{ name: 'event-list' }">List</router-link>|
+      <router-link :to="{ name: 'event-create' }">Create</router-link>|
+      <router-link :to="{ name: 'user', params: { username: 'Rafael' } }"
+        >User</router-link
+      >
     </div>
     <router-view />
   </div>
@@ -19,12 +22,10 @@
 #nav {
   padding: 30px;
 }
-
 #nav a {
   font-weight: bold;
   color: #2c3e50;
 }
-
 #nav a.router-link-exact-active {
   color: #42b983;
 }
---
+++ src/views/EventList.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Events Lits</h1>
+    <router-link :to="{ name: 'event-show', params: { id: 1 } }">Show</router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
